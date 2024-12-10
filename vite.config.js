b62420cef2b376532114bdbef97fed19182b9221
Colin <xiaoyaosha@gmail.com>
2024-12-10 14:22:05 +0800

    feat: generate open search xml

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vitePluginGenerateOpensearch from "./scripts/vite-plugin-generate-opensearch";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default ({ mode, command, ssrBuild, isSsrBuild }) => {
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd()),
+  };
+  // console.log('process.env', mode, process.env)
+  // https://vitejs.dev/config/
+  return defineConfig({
+    // base: process.env.VITE_BASE_URL,
+    plugins: [vue(), vitePluginGenerateOpensearch()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  });
+};
